ee188c9d5abe12fd2b848dc240a39d9a95bcbfcc
Joe Calleya <jcalleya@gmail.com>
2021-01-26 17:36:39 +0000

    stable connection from the NODE postgres API

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { getCollectionDocuments, createCollectionDocument, deleteCollectionDocument } from './database.js';
+import {
+    getCollectionDocuments,
+    createCollectionDocument,
+    deleteCollectionDocument
+} from './database.js';
 
 const app = express();
 app.use(cors());
@@ -11,18 +15,24 @@ app.get('/', async (request, response) => {
     const locations = await getCollectionDocuments('locations')
     response.send(locations);
 })
+
 // POST endpoint which takes the LOCATION from the request body and saves it...
 app.post('/create', async (request, response) => {
     const newLocations = request.body;
     await createCollectionDocument('locations', newLocations);
-    response.send({message: "We created this location...."})
+    response.send({
+        message: "We created this location...."
+    })
 })
+
 // DELETE endpoint which deletes the user which is sent in the request body...
 app.delete('/delete', async (request, response) => {
     const locationsToDelete = request.body;
     console.log(locationsToDelete)
     await deleteCollectionDocument('locations', locationsToDelete);
-    response.send({message: "We deleted this location...."})
+    response.send({
+        message: "We deleted this location...."
+    })
 });
 // Finally! Listen on Port 8080
 app.listen(8080);
--- database.js
+++ database.js
@@ -1,69 +1,17 @@
-// Use ES6 Imports for mongodb and our mongo client
-import mongodb from 'mongodb';
-// const mongodb = require('mongodb');
+import pkg from 'pg';
+const { Pool, Client } = pkg
 
-const mongoClient = mongodb.MongoClient;
-// ************************************************
-// HEY YOU! REPLACE "andys_db" below with the name of your database!!!!
-// ************************************************
-let dbName = "climbing";
-// connection string
 
-const uri = "mongodb+srv://testuser:password1234@cluster0.gjwh8.mongodb.net/climbing?retryWrites=true&w=majority";
-// connection string
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'shares',
+  password: 'sherlock1',
+  port: 5432
+})
+client.connect()
 
-export const getCollectionDocuments = async (collectionName) => {
-    // Connect to our database / open our connection
-    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
-    // Retrieve our collection
-    const dataCollection = await mongo.db(dbName).collection(collectionName).find({}).toArray();
-    // Close our connection
-    mongo.close();
-    return dataCollection;
-}
-export const createCollectionDocument = async (collectionName, data) => {
-    try {
-        // Connect to our database / open our connection
-        const mongo = await mongoClient.connect(uri, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-          })
-        // Create our document
-        if (!data._id) {
-            data._id = new mongodb.ObjectID().toString();
-            await mongo.db(dbName).collection(collectionName).insertOne(data)
-        } else {
-            updateCollectionDocument(collectionName, data);
-        }
-        // Close our connection
-        mongo.close();
-    } catch (e) {
-        console.log("HERE IS THE ERROR WE NEED TO LOOK AT...")
-        console.log(e);
-    }
-}
-export const updateCollectionDocument = async (collectionName, data) => {
-    // Connect to our database / open our connection
-    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
-    // Retrieve our collection
-    var myquery = { _id: new mongodb.ObjectID(data._id) };
-    var newvalues = { $set: data };
-    await mongo.db(dbName).collection(collectionName).replaceOne(
-        { _id : data._id },
-        data, 
-        { upsert: true} 
-     );
-    // Close our connection
-    mongo.close();
-}
-export const deleteCollectionDocument = async (collectionName, data) => {
-    // Connect to our database / open our connection
-    const mongo = await mongoClient.connect(uri, { useUnifiedTopology: true })
-    // Retrieve our collection
-    await mongo.db(dbName).collection(collectionName).deleteOne(
-        { name : data.name }
-     );
-    // Close our connection
-    mongo.close();
-}
+client.query('SELECT * from watchlist', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
