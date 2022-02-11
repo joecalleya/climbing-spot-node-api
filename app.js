435a710e124e2f43f4789cada5af1632394dde19
Joe <jcalleya@gmail.com>
2022-02-11 08:57:47 +0000

    stable version

--- app.js
+++ app.js
@@ -4,49 +4,81 @@ import bodyParser from 'body-parser';
 import {
   getTableData
 } from './database.js';
-import {financialsQuery, newsQuery,timeSeriesQuery} from './queries.js'
+import {financialsSummaryQuery, newsQuery, timeSeriesQuery, cashSummaryQuery, cashDetailQuery, financialsDetailQuery} from './queries.js'
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 
 // GET endpoint which returns/sends all our locations in the response
-app.get('/:dataset/:condition', async (request, response) => {
+app.get('/:dataset/:condition/:groupBy/', async (request, response) => {
   const dataset = request.params['dataset']
   const condition = request.params['condition']
-  console.log(dataset,condition)
+  console.log(dataset,condition)  
+  const groupBy = request.params['groupBy']
 
-    if (dataset == 'FINANCIALS')
+
+    if (dataset == 'FINANCIALSDETAIL')
     {
-      const result = getTableData(financialsQuery(`${condition}`))
+      getTableData(financialsDetailQuery(`${condition}`))
       .then(function (result) {
+        console.log("rows: ",result.length)
         response.send(result)
-        console.log(result.length,dataset)
+      }).catch(function (err) {
+        console.log(err)
+      });
+    }
+    else if (dataset == 'FINANCIALSSUM')
+    {
+      //console.log(financialsSummaryQuery(`${condition}`,`${groupBy}`))
+      getTableData(financialsSummaryQuery(`${condition}`,`${groupBy}`))
+      .then(function (result) {
+        console.log("rows: ",result.length)
+        response.send(result)
       }).catch(function (err) {
         console.log(err)
       });
     }
     else if (dataset == 'NEWS')
     {
-      const result = getTableData(newsQuery, '')
+      getTableData(newsQuery(`${condition}`))
       .then(function (result) {
         response.send(result)
-        console.log(result.length,dataset)
+
       }).catch(function (err) {
         console.log(err);
       })
     }
     else if (dataset == 'TIMESERIES')
     {
-      const result = getTableData(timeSeriesQuery(`${condition}`))
+      getTableData(timeSeriesQuery(`${condition}`))
       .then(function (result) {
+        console.log("rows: ",result.length)
         response.send(result)
-        console.log(result.length,dataset)
+      }).catch(function (err) {
+        console.log(err)
+      });
+    }
+    else if (dataset == 'CASHSUM')
+    {
+      getTableData(cashSummaryQuery(`${condition}`,`${groupBy}`))
+      .then(function (result) {
+        console.log("rows: ",result.length)
+        response.send(result)
+      }).catch(function (err) {
+        console.log(err)
+      });
+    }
+    else if (dataset == 'CASHDETAIL')
+    {
+      getTableData(cashDetailQuery(`${condition}`))
+      .then(function (result) {
+        console.log("rows: ",result.length)
+        response.send(result)
       }).catch(function (err) {
         console.log(err)
       });
     }
 })
 
-
-app.listen(8080);
+app.listen(8081, "192.168.1.18");
